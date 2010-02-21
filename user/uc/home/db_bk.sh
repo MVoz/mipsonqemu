c3e64d90bc64c68149ca33050a4c6ca7ad8f463e
ramen.sh <ramen.sh@gmail.com>
2010-02-21 03:02:25 +0000

    add zip file

--- user/uc/home/db_bk.sh
+++ user/uc/home/db_bk.sh
@@ -13,7 +13,7 @@ mysqldump --quick --databases $DBNAME --flush-logs --delete-master-logs --lock-a
 #echo "Dump Done" >> $LogFile
 #tar czvf $GZDumpFile $DumpFile >> $LogFile 2>&1
 #echo "[$GZDumpFile]Backup Success!" >> $LogFile
-zip  -u -P $rar_passwd $DumpFile.zip $DumpFile uc_bookmark.sdr
+zip  -u -P $rar_passwd $DumpFile.zip $DumpFile uc_bookmark.sdr config.sh
 rm -f $DumpFile 
 rm -f $LogFile
 echo "Backup Done!"
