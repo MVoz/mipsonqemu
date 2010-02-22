a42da5e0f562338f6209cdfa22ed4fd238d43162
ramen.sh <ramen.sh@c6258d96-f10f-11de-b2c3-d77de7315633>
2010-02-22 15:30:41 +0000

    test

--- user/uc/test.php
+++ user/uc/test.php
@@ -1,10 +1,8 @@
 <?php
+	@define('IN_UCHOME', TRUE);
+	@include_once('home/source/function_common.php');
 	//printf(preg_replace("/([^\:]+).*/", "&\\1", "123:192.168.115.2"));
 	//printf(preg_replace("/(\<[^\<]*\>|\r|\n|\s|\[.+?\])/is", ' ', "<hi>[123]sffsf</hi>"));
 	//printf(intval('dgdsfg'));
-	echo $_GET['id'];
-	if(isset($_GET['id']))
-		echo '1';
-	else
-		echo '0';
+	echo getstr('h<\'ello', 80, 1, 0, 1);
 ?>
