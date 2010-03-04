64c997b5581f14d8b8249ce4f6ea04ece8716063
ramen.sh <ramen.sh@c6258d96-f10f-11de-b2c3-d77de7315633>
2010-03-04 14:08:58 +0000

    modify

--- user/uc/test.php
+++ user/uc/test.php
@@ -5,4 +5,35 @@
 	//printf(preg_replace("/(\<[^\<]*\>|\r|\n|\s|\[.+?\])/is", ' ', "<hi>[123]sffsf</hi>"));
 	//printf(intval('dgdsfg'));
 	echo getstr('h<\'ello', 80, 1, 0, 1);
+	$see=empty($_GET['see'])?'':$_GET['see'];
+	if(empty($see))
+	echo 'emprty';
+	else
+	echo 'not empty';
+	
+	 $browsertype1=array(
+            'ie'=>1,
+            'firefox'=>2,
+            'opera'=>3
+        );
+
+
+function mkbrowsertab1($id)
+{
+	global $browsertype1;
+	foreach ($browsertype1 as $k => $v) {
+		echo "\$a[$k] => $v.\n";
+	}
+	echo 'xx';
+}
+    
+     mkbrowsertab1(1);
+$arr=array();
+$a=$arr['id'];
+echo $a;
+if($a)
+echo '11';
+else
+echo '22';
+
 ?>
