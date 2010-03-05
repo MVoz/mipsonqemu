5f229e0837cfe6134e9a5f191d24b5cf0f39dcde
ramen.sh@gmail.com <ramen.sh@gmail.com@c6258d96-f10f-11de-b2c3-d77de7315633>
2010-03-05 07:23:36 +0000

    modify

---
+++ trunk/user/uc/home/source/space_digg.php
@@ -0,0 +1,110 @@
+<?php
+/*
+	[UCenter Home] (C) 2007-2008 Comsenz Inc.
+	$Id: network_album.php 12078 2009-05-04 08:28:37Z zhengqingpeng $
+*/
+
+if(!defined('IN_UCHOME')) {
+	exit('Access Denied');
+}
+
+//是否公开
+if(empty($_SCONFIG['networkpublic'])) {
+	checklogin();//需要登录
+}
+include_once(S_ROOT.'./data/data_network.php');
+
+//digg
+$digglist = array();
+$cachefile = S_ROOT.'./data/cache_network_digg.txt';
+if(check_network_cache('digg')) {
+	$digglist = unserialize(sreadfile($cachefile));
+} else {
+
+	//显示数量
+	$shownum = 5;
+	
+
+	$query = $_SGLOBAL['db']->query("SELECT main.*	FROM ".tname('digg')." main
+				ORDER BY main.dateline LIMIT 0,$shownum");
+	while ($value = $_SGLOBAL['db']->fetch_array($query)) {
+		$value['subject'] = getstr($value['subject'], 50);
+		$value['cutsubject'] = getstr(trim($value['subject']), 28);
+		$digglist[] = $value;
+	}
+	if($_SGLOBAL['network']['digg']['cache']) {
+		swritefile($cachefile, serialize($digglist));
+	}
+}
+foreach($digglist as $key => $value) {
+	realname_set($value['uid'], $value['username']);
+	$value['tag'] = empty($value['tag'])?array():unserialize($value['tag']);
+	$digglist[$key] = $value;
+}
+
+
+realname_get();
+
+//最后登录名
+$membername = empty($_SCOOKIE['loginuser'])?'':sstripslashes($_SCOOKIE['loginuser']);
+$wheretime = $_SGLOBAL['timestamp']-3600*24*30;
+
+$_TPL['css'] = 'network';
+include_once template("space_digg");
+/*
+}
+*/
+//检查缓存
+function check_network_cache($type) {
+	global $_SGLOBAL;
+	
+	if($_SGLOBAL['network'][$type]['cache']) {
+		$cachefile = S_ROOT.'./data/cache_network_'.$type.'.txt';
+		$ftime = filemtime($cachefile);
+		if($_SGLOBAL['timestamp'] - $ftime < $_SGLOBAL['network'][$type]['cache']) {
+			return true;
+		}
+	}
+	return false;
+}
+
+//获得SQL
+function mk_network_sql($type, $ids, $crops, $days, $orders) {
+	global $_SGLOBAL;
+	
+	$nt = $_SGLOBAL['network'][$type];
+	
+	$wherearr = array('1');
+	//指定
+	foreach ($ids as $value) {
+		if($nt[$value]) {
+			$wherearr[] = "main.{$value} IN (".$nt[$value].")";
+		}
+	}
+	
+	//范围
+	foreach ($crops as $value) {
+		$value1 = $value.'1';
+		$value2 = $value.'2';
+		if($nt[$value1]) {
+			$wherearr[] = "main.{$value} >= '".$nt[$value1]."'";
+		}
+		if($nt[$value2]) {
+			$wherearr[] = "main.{$value} <= '".$nt[$value2]."'";
+		}
+	}
+	//时间
+	foreach ($days as $value) {
+		if($nt[$value]) {
+			$daytime = $_SGLOBAL['timestamp'] - $nt[$value]*3600*24;
+			$wherearr[] = "main.{$value}>='$daytime'";
+		}
+	}
+	//排序
+	$order = in_array($nt['order'], $orders)?$nt['order']:array_shift($orders);
+	$sc = in_array($nt['sc'], array('desc','asc'))?$nt['sc']:'desc';
+	
+	return array('wherearr'=>$wherearr, 'order'=>$order, 'sc'=>$sc);
+}
+
+?>
