d73fcc0c53a84b0108fd7772b23c59efa2a02823
ramen.sh <ramen.sh@c6258d96-f10f-11de-b2c3-d77de7315633>
2010-03-11 12:42:51 +0000

    delete

--- user/uc/home/db_bk.sh
+++ user/uc/home/db_bk.sh
@@ -6,15 +6,13 @@ DATE=`date +%Y%m%d`
 #cd $BakDir
 #DumpFile=$DATE.sql
 DumpFile=uc_db.sql
+rm -fr $DumpFile.sql.zip
 #GZDumpFile=$DATE.sql.tgz
 GZDumpFile=$DumpFile.tgz
 
 mysqldump --quick --databases $DBNAME --flush-logs --delete-master-logs --lock-all-tables --default-character-set=utf8 -u $DB_USER -p$mysql_passwd > $DumpFile
-#echo "Dump Done" >> $LogFile
-#tar czvf $GZDumpFile $DumpFile >> $LogFile 2>&1
-#echo "[$GZDumpFile]Backup Success!" >> $LogFile
 zip  -u -P $rar_passwd $DumpFile.zip $DumpFile uc_bookmark.sdr config.sh  bm_logo.psd
 rm -f $DumpFile 
 rm -f $LogFile
-svn commit -m "$DATA"
+#svn commit -m "$DATA"
 echo "Backup Done!"
