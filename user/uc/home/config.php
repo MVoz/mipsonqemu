92e8f0a08d68afd7beaa3299bfa386182c78c6c4
ramen.sh <ramen.sh@gmail.com>
2010-03-11 07:47:47 +0000

    edit

--- user/uc/home/config.php
+++ user/uc/home/config.php
@@ -47,7 +47,7 @@ $_SC['link_delete_uid']=11;
 
 //length
 $_SC['subject_length']=256;
-$_SC['subject_nbox_title_length']=32;
+$_SC['subject_nbox_title_length']=56;
 
 $_SC['description_length']=512;
 $_SC['description_nbox_title_length']=128;
