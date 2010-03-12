32f115051a40cd4f0e939886bd7a642c2a88eab3
ramen.sh@gmail.com <ramen.sh@gmail.com@c6258d96-f10f-11de-b2c3-d77de7315633>
2010-03-12 03:28:50 +0000

    $logdate

--- trunk/user/uc/home/source/space_bookmark.php
+++ trunk/user/uc/home/source/space_bookmark.php
@@ -24,18 +24,6 @@ $wheretime = $_SGLOBAL['timestamp']-3600*24*30;
 
 $_TPL['css'] = 'network';
 include_once template("space_bookmark");
-//检查缓存
-function check_network_cache($type) {
-	global $_SGLOBAL;
-	
-	if($_SGLOBAL['network'][$type]['cache']) {
-		$cachefile = S_ROOT.'./data/cache_network_'.$type.'.txt';
-		$ftime = filemtime($cachefile);
-		if($_SGLOBAL['timestamp'] - $ftime < $_SGLOBAL['network'][$type]['cache']) {
-			return true;
-		}
-	}
-	return false;
-}
+
 
 ?>
