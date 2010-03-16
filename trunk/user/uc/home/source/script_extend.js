80250fbee14421631ddf8b99921db4cf2f6a5bf6
ramen.sh@gmail.com <ramen.sh@gmail.com@c6258d96-f10f-11de-b2c3-d77de7315633>
2010-03-16 01:42:31 +0000

    $logdate

---
+++ trunk/user/uc/home/source/script_extend.js
@@ -0,0 +1,67 @@
+function getbmview(type) {
+		ajaxgetex('space.php?do=bookmark&op='+type, 'bmcontent','relatedcontent','relatehtm');	    
+}
+
+function getlinkview(type) {
+	ajaxget('space.php?do=link&op='+type, 'bmcontent');
+}
+
+function getbmfromid(id) {
+	if(id==0)
+	        jQuery('#menu ul').hide();
+	ajaxgetex('space.php?do=bookmark&op=browser&groupid='+id+'&browserid=$browserid', 'bmcontent','relatedcontent','relatehtm');	
+}
+
+function ajaxresponse(objname, data) {
+	var x = new Ajax('XML', objname);
+	x.get('cp.php?ac=bookmark&' + data, function(s){
+	});
+}
+
+function updatevisitstat(id) {
+	ajaxresponse('visitstat', 'op=updatevisitstat&bmid=' + id);
+}
+
+function getrelatedlinkfromid(id) {
+	ajaxget('cp.php?ac=link&op=relate&linkid='+id, 'relatedsitecontent');    
+}
+
+var lastSecCode='';
+
+function checkSeccode() {
+		var seccodeVerify = $('seccode').value;
+		if(seccodeVerify == lastSecCode) {
+			return;
+		} else {
+			lastSecCode = seccodeVerify;
+		}
+		ajaxresponse('checkseccode', 'op=checkseccode&seccode=' + (is_ie && document.charset == 'utf-8' ? encodeURIComponent(seccodeVerify) : seccodeVerify));
+}
+
+function ajaxresponse(objname, data) {
+		var x = new Ajax('XML', objname);
+		x.get('cp.php?ac=link&' + data, function(s){
+			var obj = $(objname);
+			s = trim(s);
+			if(s.indexOf('succeed') > -1) {
+				obj.style.display = '';
+				obj.innerHTML = '<img src="image/check_right.gif" width="13" height="13">';
+				obj.className = "warning";
+				$('btnsubmit').disabled=false;
+				$('btnsubmit').className="submit";
+			} else {
+				warning(obj, s);
+				$('btnsubmit').disabled=true;
+				$('btnsubmit').className="submit_disabled";
+			}
+		});
+}
+
+function warning(obj, msg) {
+		if((ton = obj.id.substr(5, obj.id.length)) != 'password2') {
+			$(ton).select();
+		}
+		obj.style.display = '';
+		obj.innerHTML = '<img src="image/check_error.gif" width="13" height="13"> &nbsp; ' + msg;
+		obj.className = "warning";
+}
