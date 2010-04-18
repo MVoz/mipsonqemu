e833c6ba4b4acd954186a9cc3696b97c7e9845e2
ramen.sh <ramen.sh@c6258d96-f10f-11de-b2c3-d77de7315633>
2010-04-18 15:10:10 +0000

    $logdate

---
+++ user/uc/home/source/space_login.php
@@ -0,0 +1,29 @@
+<?php
+/*
+	[UCenter Home] (C) 2007-2008 Comsenz Inc.
+	$Id: network_album.php 12078 2009-05-04 08:28:37Z zhengqingpeng $
+*/
+
+if(!defined('IN_UCHOME')) {
+	exit('Access Denied');
+}
+
+//是否公开
+if(empty($_SCONFIG['networkpublic'])) {
+	checklogin();//需要登录
+}
+
+include_once(S_ROOT.'./source/space_bookmark_show.php');
+
+//今日热荐
+include_once(S_ROOT.'./source/todayhot.php');
+
+//最后登录名
+$membername = empty($_SCOOKIE['loginuser'])?'':sstripslashes($_SCOOKIE['loginuser']);
+$wheretime = $_SGLOBAL['timestamp']-3600*24*30;
+
+$_TPL['css'] = 'network';
+include_once template("space_login");
+
+
+?>
