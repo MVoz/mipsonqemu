1fc3d9257ed47695f8edb456ecbf2065dc406d48
ramen.sh <ramen.sh@gmail.com>
2010-05-14 10:30:17 +0000

    $logdate

--- user/uc/home/data/data_mail.php
+++ user/uc/home/data/data_mail.php
@@ -2,8 +2,14 @@
 if(!defined('IN_UCHOME')) exit('Access Denied');
 $_SGLOBAL['mail']=Array
 	(
-	'mailsend' => 1,
+	'mailsend' => 2,
 	'maildelimiter' => '0',
-	'mailusername' => 1
+	'mailusername' => 1,
+	'server' => 'smtp.sohu.com',
+	'port' => 25,
+	'auth' => '0',
+	'from' => 'myqsq@sohu.com',
+	'auth_username' => 'myqsq',
+	'auth_password' => 98122130
 	)
 ?>
