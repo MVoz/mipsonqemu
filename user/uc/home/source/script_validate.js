c064d6519a9bd0b046d7393bcdb84ef9e69cd0af
ramen.sh <ramen.sh@c6258d96-f10f-11de-b2c3-d77de7315633>
2010-05-17 14:19:36 +0000

    edit

---
+++ user/uc/home/source/script_validate.js
@@ -0,0 +1,30 @@
+function bookmark_validate(obj) {
+    var subject = $('subject');
+    if (subject) {
+    	var slen = strlen(subject.value);
+        if (slen < 1 || slen > 80) {
+            alert("标题长度(1~80字符)不符合要求");
+            subject.focus();
+            return false;
+        }
+    }
+    
+    if($('seccode')) {
+		var code = $('seccode').value;
+		var x = new Ajax();
+		x.get('cp.php?ac=common&op=seccode&code=' + code, function(s){
+			s = trim(s);
+			if(s.indexOf('succeed') == -1) {
+				alert(s);
+				$('seccode').focus();
+           		return false;
+			} else {
+				obj.form.submit();
+				return true;
+			}
+		});
+    } else {
+     	obj.form.submit();
+    	return true;
+    }
+}
