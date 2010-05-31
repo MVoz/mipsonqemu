ef6316c05396699b1ec2966aeec91e136fe5c0a2
ramen.sh <ramen.sh@c6258d96-f10f-11de-b2c3-d77de7315633>
2010-05-31 14:32:36 +0000

    edit

---
+++ user/uc/home/source/cron/everydayhot.php
@@ -0,0 +1,15 @@
+<?php
+/*
+	[UCenter Home] (C) 2007-2008 Comsenz Inc.
+	$Id: cleanfeed.php 12681 2009-07-15 05:24:47Z liguode $
+*/
+
+if(!defined('IN_UCHOME')) {
+	exit('Access Denied');
+}
+
+
+include_once(S_ROOT.'./source/function_cache.php');
+everydayhot_cache();
+
+?>
