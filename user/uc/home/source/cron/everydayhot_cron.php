bf989f613e3fc8af332630272ca366f52a8da358
ramen.sh <ramen.sh@gmail.com>
2010-06-03 13:17:01 +0000

    $logdate

--- user/uc/home/source/cron/everydayhot_cron.php
+++ user/uc/home/source/cron/everydayhot_cron.php
@@ -3,6 +3,7 @@
 	[UCenter Home] (C) 2007-2008 Comsenz Inc.
 	$Id: cleanfeed.php 12681 2009-07-15 05:24:47Z liguode $
 */
+include_once('/var/www/uc/home/common.php');
 if(!defined('IN_UCHOME')) {
 	exit('Access Denied');
 }
