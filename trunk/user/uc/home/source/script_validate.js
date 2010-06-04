efcba216153c610c172d5b3a5f4b21c33ba063bf
ramen.sh <ramen.sh@c6258d96-f10f-11de-b2c3-d77de7315633>
2010-06-04 15:41:51 +0000

    edit

--- trunk/user/uc/home/source/script_validate.js
+++ trunk/user/uc/home/source/script_validate.js
@@ -1,32 +1,90 @@
-function bookmark_validate(obj) {
-    var subject = $('subject');
-    if (subject) {
-    	var slen = strlen(subject.value);
-        if (slen < 1 || slen > 80) {
-            alert("±ÍÃ‚≥§∂»(1~80◊÷∑˚)≤ª∑˚∫œ“™«Û");
-            subject.focus();
+
+function email_validate(value)
+{
+	return /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i.test(trim(value));
+}
+function url_validate(value)
+{
+	return  /^(https?|ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i.test(trim(value));
+}
+function number_validate(value)
+{
+	return /^\d+$/.test(trim(value));
+}
+function range_validate(value,min,max)
+{
+	return ((/^\d+$/.test(trim(value)))&&(parseInt(value)<=max)&&(parseInt(value)>=min));
+}
+function illegachar_validate(value)
+{
+   return reg = /['"\\/]/i.test(trim(value)) ; 
+}
+function maxlen_validate(value,max)
+{
+  return(strlen(trim(value))<=max);
+}
+function rangelen_validate(value,min,max)
+{
+  return((strlen(trim(value))<=max)&&(strlen(trim(value))>=min));
+}
+/*
+ include UTF-8
+*/
+function getLength(value)
+{
+	value=trim(value)
+	var length = value.length;
+    for(var i = 0; i < value.length; i++){
+        if(value.charCodeAt(i) > 127){
+            length++;
+        }
+    }
+	return length;
+}
+
+function check_subject(id,min,max)
+{
+	if ($(id)) {
+		if(!rangelen_validate($(id).value,min,max))    	
+		{
+            warning($(id+'_tip'),"Ê†áÈ¢òÈïøÂ∫¶("+min+"~"+max+"Â≠óÁ¨¶)‰∏çÁ¨¶ÂêàË¶ÅÊ±Ç");
             return false;
         }
     }
-    
-    if($('seccode')) {
+	return true;
+}
+function check_url(id,min,max)
+{
+	if ($(id)) {
+		if(!url_validate($(id).value)||!rangelen_validate($('address').value,min,max))    	
+			{
+				warning($(id+'_tip'),"ÁΩëÂùÄ("+min+"~"+max+"Â≠óÁ¨¶)‰∏çÁ¨¶ÂêàË¶ÅÊ±Ç");
+				return false;
+			}
+    }
+	return true;
+}
+function check_seccode()
+{
+	if($('seccode')) {
 		var code = $('seccode').value;
 		var x = new Ajax();
 		x.get('cp.php?ac=common&op=seccode&code=' + code, function(s){
 			s = trim(s);
 			if(s.indexOf('succeed') == -1) {
-				alert(s);
-				$('seccode').focus();
-           		return false;
-			} else {
-				obj.form.submit();
-				return true;
-			}
+				 warning($('checkseccode'),s);
+           		 return false;			
 		});
-    } else {
-     	obj.form.submit();
-    	return true;
     }
+	return true;
+}
+function bookmark_validate(obj) {
+
+	if(!check_subject('subject',1,1024)||!check_subject('address',1,1024)||!check_seccode())
+			return false;
+	 
+   	obj.form.submit();
+    return true;    
 }
 
 function report_validate(obj) {
