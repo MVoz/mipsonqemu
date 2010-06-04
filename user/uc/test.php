46bb356d5f1760186cde822eaf48e3daa7b10df4
ramen.sh <ramen.sh@gmail.com>
2010-06-04 09:50:44 +0000

    edit

--- user/uc/test.php
+++ user/uc/test.php
@@ -17,7 +17,11 @@ function handleUrlString($url)
 }
 echo handleUrlString("http://www.sohu.com///////");
 echo empty($_GET['uid'])?1:intval($_GET['uid']);
-<!--
-http://nowdownloadall.com/join.asp?PID=0cdfe76c-1849-49fd-acb3-44e095532314&ts=6/1/2010%2010:28:27%20PM&q=Understanding%20IPv6%20zip&cr=1
--->
+echo HELLO;
+//http://nowdownloadall.com/join.asp?PID=0cdfe76c-1849-49fd-acb3-44e095532314&ts=6/1/2010%2010:28:27%20PM&q=Understanding%20IPv6%20zip&cr=1
+
+//http://quickrpickr.com/ 
+
+//img src="http://lh4.ggpht.com/_QeLyLM_38SU/TAX3i8Jzg9I/AAAAAAAAAGo/r8QD8uaZSZs/s800/1.jpg" />
+//http://hi.baidu.com/viste_happy/blog/item/699a07fc72e84849d7887d2a.html
 ?> 
