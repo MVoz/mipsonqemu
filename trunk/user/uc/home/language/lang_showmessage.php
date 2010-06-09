e05ef66225e2c07e5c5dcafcad85e8532fca9596
ramen.sh@gmail.com <ramen.sh@gmail.com@c6258d96-f10f-11de-b2c3-d77de7315633>
2010-06-09 11:47:07 +0000

    $logdate

--- trunk/user/uc/home/language/lang_showmessage.php
+++ trunk/user/uc/home/language/lang_showmessage.php
@@ -444,7 +444,7 @@ $_SGLOBAL['msglang'] = array(
 	'space_has_been_locked' => '空间已被锁定无法访问，如有疑问请联系管理员',
 	//source/cpbmdir.php
 	'err_bmdir_add_overflow' => '超出目录分类的最大深度' ,
-	'err_bookmark_add_overflow' => '超出目录书签的最大允许数' ,
+	'err_bookmark_add_overflow' => '超出目录书签的最大允许数' 
 	
 );
 
