bb49b60b27c11e809db49c244a171313bf96145a
ramen.sh <ramen.sh@gmail.com>
2010-06-09 11:47:15 +0000

    $logdate

--- user/uc/home/source/suggest.js
+++ user/uc/home/source/suggest.js
@@ -53,40 +53,10 @@ function searchSubmit(a) {
     }
 };
 
-function getOldSearchTabCookieName() {
-    return "schtab"
-};
 
-function getOldSearchTab() {
-    var a = getCookie(getOldSearchTabCookieName());
-    if (a == null || a == "") return false;
-    var b = a.split(",");
-    if (b[1] != 0) return false;
-    if (b[0] > 1) return false;
-    return b[0]
-};
-
-function highlightSearchTab() {
-    var a = getSearchTabByIndex(0);
-    var b = getOldSearchTab();
-    if (b === false) displayTab("web_page", a);
-    else {
-        delCookie(getOldSearchTabCookieName());
-        displayTab("web_page", b)
-    }
-};
 var j;
 
-function init() {
-    var a = getObj("searchTab").getElementsByTagName("strong");
-    j = a.length;
-    for (var b = 0; b < j; b++) a[b].onclick = function () {
-        if (this.id == getSearchTabCookieName()) {
-            var a = getCookie(this.id);
-            displayTab(this.id, a)
-        } else displayTab(this.id, /(wenda|shopping)/.test(this.id)?0:1)
-    }
-};
+
 var searchs = {
     web_page: [
         ["\u7f51\u9875", "http://www.baidu.com/s", "wd", "\u767e\u5ea6", -1, "http://www.baidu.com/index.php",1,"ie:utf-8"],
@@ -132,129 +102,6 @@ var searchs = {
 		
 };
 
-function setSearchTabClass(a) {
-    for (var b in searchs) getObj(b).className = b == a ? "bStyle" : ""
-};
-
-function searchChange(a, b) {
-    getObj("hidpar").innerHTML = "";
-    getObj("searchForm").action = searchs[a][b][1];
-    getObj("searchkey").name = searchs[a][b][2];
-    getObj("searchBtn").value = searchs[a][b][3] + "\u641c\u7d22";
-    getObj("setUrl").style.backgroundPosition = "0 " + searchs[a][b][4] + "px";
-    getObj("setUrl").href = searchs[a][b][5];
-    setSearchTabClass(a)
-};
-
-function setSearchRadio(a, b) {
-    var c = searchs[a],
-        oSearch = getObj("selectId"),
-        radioArr = [];
-    for (var i = 0, len = c.length; i < len; i++) {
-            radioArr[i] = '<label for="se' + i + '">' + "<input onclick=\"displayTab2('" + a + "'," + i + ')" ' + (i == b ? "checked" : "") + ' type="radio" name="searchRadio" id="se' + i + '"/>' + "<span>" + c[i][3] + "</span></label>"
-        }
-    oSearch.innerHTML = radioArr.join("")
-};
-
-function setSearchHidden(a, b) {
-    if (searchs[a][b][6] != null) {
-        var c = searchs[a][b][6].split(";");
-        for (var d = 0, len = c.length; d < len; d++) {
-            var e = c[d].split(":");
-            var f = document.createElement("INPUT");
-            f.type = "hidden";
-            f.name = e[0];
-            f.id = e[0];
-            f.value = e[1];
-            getObj("hidpar").appendChild(f)
-        }
-    }
-    if (a == "shopping" && b == 1) getObj("searchForm").onsubmit = function () {
-        if (getObj("backUrl")) {
-            getObj("backUrl").value = "http%3A//search.dangdang.com/search.aspx%3Fkey%3D" + getObj("searchkey").value;
-            return true
-        }
-    }
-};
-
-function displayTab(a, b) {
-    searchChange(a, b);
-    setSearchRadio(a, b);
-    setSearchHidden(a, b);
-    if (a == "web_page") setSearchTab(b)
-};
-
-function displayTab2(a, b) {
-    searchChange(a, b);
-    setSearchHidden(a, b);
-    if (a == "web_page") setSearchTab(b)
-};
-
-function setPage() {
-    if ("http://hao.360.cn/set.html" != getObj("setPageIfrm").src) getObj("setPageIfrm").src = "http://hao.360.cn/set.html";
-    if ("none" == getObj("set").style.display) getObj("set").style.display = "";
-    else getObj("set").style.display = "none"
-};
-
-function col() {
-    getObj("setBt").className = "setBtn";
-    getObj("set").style.display = "none"
-};
-var g_mode_standard = "0",
-    g_mode_search = "1",
-    g_mode_hao360 = "2";
-
-function UseMode(a) {
-        try {
-            window.location.href = a === g_mode_search ? "se:home" : a === g_mode_hao360 ? "http://hao.360.cn" : "se:home"
-        } catch (b) {}
-    };
-
-function SetUseMode(a) {
-        try {
-            var b = external.twGetSecurityID(window);
-            b = external.SetOptionValue(b, "option", "UseSimple", a);
-            UseMode(a)
-        } catch (c) {}
-    };
-
-function GetUseMode() {
-        try {
-            var a = external.twGetSecurityID(window),
-                b = external.GetOptionValue(a, "option", "UseSimple");
-            if (b == null || b == "undefined" || b == "") b = g_mode_standard;
-            return b
-        } catch (c) {
-            return g_mode_standard
-        }
-    };
-
-function GetSeVer() {
-        try {
-            return external.twGetVersion(external.twGetSecurityID(window))
-        } catch (a) {
-            return "3.0.0.1"
-        }
-    };
-
-function IsFromSehome() {
-        if (window.location.href.indexOf("src=sehomeb") > 0) return true;
-        return false
-    };
-
-function showSehomeMode() {
-        try {
-            if (GetSeVer() >= "3.1.0.3" && IsFromSehome() && getObj("semodeBox") != null) getObj("semodeBox").style.display = ""
-        } catch (a) {}
-    };
-
-function getRight() {
-        if (getCookie("360WEBPW") == 1) {
-            getObj("setPageMenu").style.marginRight = screen.availWidth > 1020 && screen.availWidth < 1124 ? "136px" : "";
-            getObj("set").style.right = screen.availWidth > 1020 && screen.availWidth < 1124 ? "136px" : ""
-        }
-    };
-
 function getSearchTabCookieName() {
         return "web_page"
     };
@@ -268,40 +115,6 @@ function getSearchTabByIndex(a) {
         if (a == null || a == "") return 1;
         return a
     };
-window.onload = function () {
-     //   getRight();
-    //    showSehomeMode()
-    };
-//init();
-//highlightSearchTab();
-
-
-function do_s() {
-        var $ = getObj("keyword").value;
-        var a = encodeURIComponent($);
-        var b = document.getElementsByName("msrg");
-        if (b[0].checked) window.open("http://www.baidu.com/s?ie=utf-8&wd=" + a);
-        if (b[1].checked) window.open("http://www.google.cn/search?client=aff-360daohang&q=" + a);
-        if (b[2].checked) window.open("http://www.qihoo.com/wenda.php?do=search&area=2&src=bbs&kw=" + $);
-        if (b[3].checked) window.open("http://www.google.cn/books?client=aff-360daohang&q=" + a);
-        if (b[4].checked) window.open("http://mp3.baidu.com/m?ie=utf-8&f=ms&ct=134217728&word=" + $);
-        if (b[5].checked) window.open("http://video.gougou.com/search?id=&s=" + $);
-        if (b[6].checked) window.open("http://movie.gougou.com/search?id=&search=" + $);
-        if (b[7].checked) window.open("http://image.baidu.com/i?ie=utf-8&lm=-1&ct=201326592&word=" + a);
-        if (b[8].checked) window.open("http://shenghuo.google.cn/shenghuo/search?client=aff-360daohang&q=" + a);
-        if (b[9].checked) window.open("http://search8.taobao.com/browse/search_auction.htm?search_type=auction&commend=all&at_topsearch=1&user_action=initiative&q=" + $);
-        if (b[10].checked) window.open("http://www.nciku.com/search/all/" + $);
-        if (b[11].checked) window.open("http://ditu.google.cn/maps?q=" + a);
-        if (b[12].checked) window.open("http://zhidao.baidu.com/q?tn=ikaslist&ct=17&pt=360se_ik&word=" + $);
-        if (b[13].checked) window.open("http://soft.gougou.com/search?id=&client=aff-360daohang&search=" + $);
-        return false
-    }
-
-
-function getObj(a) {
-        return document.getElementById(a)
-    };
-
 function getCookie(a) {
         var b;
         var c = new RegExp("(^| )" + a + "=([^;]*)(;|$)");
@@ -328,153 +141,13 @@ function delCookie(a) {
                 document.cookie = a + "=" + escape(cval) + ";expires=" + c.toGMTString() + ";path=/;"
             }
     };
+function getObj(a) {
+        return document.getElementById(a)
+    };
 
 function setDisplay(a, b) {
         if (getObj(a)) getObj(a).style.display = b ? "block" : "none"
     };
 
-function loadJs(a, b, c) {
-        var d = document.getElementsByTagName("head")[0],
-            oScript = null;
-        if (getObj(a) == null) {
-                oScript = document.createElement("script");
-                oScript.setAttribute("type", "text/javascript");
-                oScript.setAttribute("src", b);
-                oScript.setAttribute("id", a);
-                if (c != null) oScript.onload = oScript.onreadystatechange = function () {
-                    if (oScript.ready) return false;
-                    if (!oScript.readyState || oScript.readyState == "loaded" || oScript.readyState == "complete") {
-                        oScript.ready = true;
-                        c()
-                    }
-                };
-                d.appendChild(oScript);
-                return true
-            } else {
-                if (c != null) {
-                    c();
-                    return true
-                }
-                return false
-            }
-    };
 
-function checkMail(a) {
-        loadJs("mail_js", mailJsPath, function () {
-            _s.check(a)
-        });
-        return false
-    }; -
-function () {
-        var b = getObj('tag');
-        b && (b.onclick = function (e) {
-            e = e || event;
-            var t = e.srcElement || e.target;
-            if (/span|li/i.test(t.tagName)) {
-                var a = t.id.match(/\d/)[0],
-                    num = getObj("conBox").getElementsByTagName("div"),
-                    urlArr = {
-                        2: "http://rd.qihoo.com/qd.html?u=http%253A%252F%252Fhao.360.cn%252FgupiaocaipiaoTab.html&t=1264384954&c=1&a=9&p=6372&s=263bada48c52805af36481fbbf058558",
-                        3: "http://rd.qihoo.com/qd.html?u=http%253A%252F%252Fhao.360.cn%252Fgametab.html&t=1265870776&c=1&a=9&p=6420&s=a13c6c167948b4dca16ab3d3a5c49304",
-                        4: "http://d.hao.360.cn/listMyBookMark.php",
-                        5: "http://hao.360.cn/unclosedlist2.html",
-                        6: "http://rd.qihoo.com/qd.html?u=http%253A%252F%252Fhao.360.cn%252Fmzgouwutab.html&t=1264474024&c=1&a=9&p=6388&s=8449a9b186277cf90694fb0378b9f7f2",
-                        7: "http://hao.360.cn/dailysoftware.html"
-                    };
-                for (var i = 1; i <= num.length; i++) if (getObj("con" + i) && getObj("m" + i)) {
-                        getObj("con" + i).style.display = "none";
-                        getObj("m" + i).className = ""
-                    };
-                if (getObj("con" + a) && getObj("m" + a)) {
-                        if (getObj("ifrm" + a)) getObj("ifrm" + a).src = urlArr[a];
-                        getObj("con" + a).style.display = "block";
-                        getObj("m" + a).className = "at"
-                    }
-            }
-        })
-    }();
-
-function statistic(a) {
-        for (var i = 0, ci, p = [a.action + '?'], inps = a.getElementsByTagName('input'); ci = inps[i++];) {
-            ci.type != 'submit' && (p.push(ci.name + '=' + ci.value))
-        };
-        (new Image).src = "http://clkstat.qihoo.com/stat.gif?_pdt=mph&_pg=2609&_bd=" + a.name + "&_u=" + encodeURIComponent(p.join('')) + "&_r=" + (+new Date) + "&_t=2";
-        return true
-    }; -
-function () {
-        var a = getCookie("360WEBINDEXCK"),
-            url = location + '',
-            param = url.split('?')[1];
-        if (a && url.indexOf(a) == -1 && /^http:\/\/hao\.360\.cn\//i.test(a)) {
-                location.href = a + (param ? (/\?/.test(a) ? '&' : '?') + param : '')
-            }
-    }();
-if (getCookie("360WEBPW") == 1) getObj("widthS").href = widthCssPath;
-
-function getCookie(a) {
-        var b, reg = new RegExp("(^| )" + a + "=([^;]*)(;|$)");
-        if (b = document.cookie.match(reg)) return unescape(b[2]);
-        else return null
-    };
-
-function setCookie(a, b) {
-        var c = arguments[2] ? arguments[2] : 365 * 24 * 60 * 60 * 1000;
-        var d = new Date();
-        d.setTime(d.getTime() + c);
-        document.cookie = a + "=" + escape(b) + ";domain=hao.360.cn;path=/;expires=" + d.toGMTString()
-    };
-
-function visit(a) {
-        a = a ? a : window.event;
-        var s = (a.target) ? a.target : a.srcElement;
-        if (s.tagName == "A" && s.parentNode.tagName == "LI" && s.href.indexOf("http://") == 0 && s.href.indexOf("http://" + location.host) != 0) setVisitCookie(s)
-    };
-
-function setVisitCookie(a) {
-        cookieValue = a.innerHTML + "|" + a.href;
-        if (!isVisited(cookieValue)) {
-            visitedArr = get360VisitedWeb();
-            expire = 1000 * 3600 * 24 * 30;
-            if (visitedArr) {
-                visited = '';
-                var b = 20;
-                var c = visitedArr.length >= b ? b - 1 : visitedArr.length;
-                for (var i = 0; i < c; i++) {
-                    if (visitedArr[i] != '') visited = visited == '' ? visitedArr[i] : visited + ";" + visitedArr[i]
-                }
-                setCookie(getVisitedCookieName(), cookieValue + ";" + visited, expire)
-            } else {
-                setCookie(getVisitedCookieName(), cookieValue, expire)
-            }
-        }
-        return
-    };
-Array.prototype.in_array = function (e) {
-        for (var i = 0; i < this.length; i++) {
-            if (this[i] && e == this[i]) return true
-        }
-        return false
-    };
-
-function getVisited() {
-        return getCookie(getVisitedCookieName())
-    };
-
-function get360VisitedWeb() {
-        visited = getVisited();
-        if (!visited) return null;
-        visitedArr = visited.split(";");
-        if (visitedArr) return visitedArr;
-        return null
-    };
-
-function isVisited(a) {
-        visitedArr = get360VisitedWeb();
-        if (visitedArr) return visitedArr.in_array(a);
-        return false
-    };
-
-function getVisitedCookieName() {
-        return "360WebUserVisited"
-    }
 document.onclick = visit;
