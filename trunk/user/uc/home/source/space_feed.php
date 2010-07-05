4a7cc78b21bdfbab2168578c2ae1ff3b8e29ccbd
ramen.sh <ramen.sh@c6258d96-f10f-11de-b2c3-d77de7315633>
2010-07-05 14:31:33 +0000

    $logdate

--- trunk/user/uc/home/source/space_feed.php
+++ trunk/user/uc/home/source/space_feed.php
@@ -11,8 +11,33 @@ $sitefeed_list = array();
 
 $query = $_SGLOBAL['db']->query("SELECT * FROM ".tname('feed')." ORDER BY dateline DESC");
 while ($value = $_SGLOBAL['db']->fetch_array($query)) {
+			
+			
+			switch($value['icon'])
+			{
+				case 'digg':
+						$q = $_SGLOBAL['db']->query("SELECT * FROM ".tname('digg')." where diggid=".$value['id']);
+						if($s = $_SGLOBAL['db']->fetch_array($q))
+						{
+						   $s['tag'] = empty($s['tag'])?array():unserialize($s['tag']);
+						   $value['relate']= $s;
+						} else
+						continue;
+					break;
+				case 'link':
+						$q = $_SGLOBAL['db']->query("SELECT * FROM ".tname('link')." where linkid=".$value['id']);
+						if($s = $_SGLOBAL['db']->fetch_array($q))
+						{
+						   include_once(S_ROOT.'./source/function_link.php');
+						   $s['link_tag'] = convertlinktag($s['linkid'],$s['link_tag']);
+						   $s['link_tag'] = empty($s['link_tag'])?array():unserialize($s['link_tag']);
+						   $value['relate']= $s;
+						} else
+						continue;
+					break;
+			}
 			realname_set($value['uid'], $value['username']);
-			$sitefeed_list[] = $value; 		
+			$sitefeed_list[] = $value; 
 };
 
 include_once template("space_feed");
