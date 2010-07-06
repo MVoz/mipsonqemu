06e9f863d996b032ea159bfe49b8fda625179040
ramen.sh <ramen.sh@gmail.com>
2010-07-06 13:01:20 +0000

    $logdate

--- user/uc/home/source/space_feed.php
+++ user/uc/home/source/space_feed.php
@@ -7,12 +7,30 @@
 if(!defined('IN_UCHOME')) {
 	exit('Access Denied');
 }
+
+//根据user来查看
+$uid = empty($_GET['uid'])?0:intval($_GET['uid']);
+//显示数量
+//$shownum = $_SC['per_page_num'];
+$shownum = 5;	
+//获取总条数
+$page=empty($_GET['page'])?0:intval($_GET['page']);
+$perpage=$shownum;
+$start=$page?(($page-1)*$perpage):0;
+$theurl="space.php?do=$do";
+
 $sitefeed_list = array();
 
-$query = $_SGLOBAL['db']->query("SELECT * FROM ".tname('feed')." ORDER BY dateline DESC");
+
+$wherearr='';
+if(!empty($uid))
+	 $wherearr=' where main.uid='.$uid;
+
+$count = $_SGLOBAL['db']->result($_SGLOBAL['db']->query("SELECT COUNT(*) FROM ".tname('feed')),0);
+$query = $_SGLOBAL['db']->query("SELECT main.*	FROM ".tname('feed')." main ".$wherearr." ORDER BY main.dateline desc LIMIT $start,$shownum");
+
 while ($value = $_SGLOBAL['db']->fetch_array($query)) {
-			
-			
+					
 			switch($value['icon'])
 			{
 				case 'digg':
@@ -40,6 +58,8 @@ while ($value = $_SGLOBAL['db']->fetch_array($query)) {
 			$sitefeed_list[] = $value; 
 };
 
+
+$feedmulti = multi($count, $perpage, $page, $theurl,'bmcontent','bmcontent',1);
 include_once template("space_feed");
 
 ?>
