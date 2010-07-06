a17bc6f6be93b51738482d4bd6ec3a443f75cf85
ramen.sh <ramen.sh@c6258d96-f10f-11de-b2c3-d77de7315633>
2010-07-06 15:49:12 +0000

    edit

--- trunk/user/uc/home/source/sitefeed.php
+++ trunk/user/uc/home/source/sitefeed.php
@@ -10,9 +10,32 @@ if(!defined('IN_UCHOME')) {
 $sitefeed_list = array();
 //$sitefeedstarttime = $_SGLOBAL['timestamp'] - $_SCONFIG['feedhotday']*3600*24;
 //$query = $_SGLOBAL['db']->query("SELECT * FROM ".tname('feed')." USE INDEX(hot) WHERE dateline>='$hotstarttime' ORDER BY hot DESC LIMIT 0,10");
-$query = $_SGLOBAL['db']->query("SELECT * FROM ".tname('feed')." ORDER BY dateline DESC LIMIT 0,5");
-while ($value = $_SGLOBAL['db']->fetch_array($query)) {
-			realname_set($value['uid'], $value['username']);
-			$sitefeed_list[] = $value; 		
-};
+
+
+
+$cachefile = S_ROOT.'./data/cache_feed.txt';
+if(check_feed_cache()) {
+	$sitefeed_list = unserialize(sreadfile($cachefile));
+} else {
+	 $query = $_SGLOBAL['db']->query("SELECT * FROM ".tname('feed')." ORDER BY dateline DESC LIMIT 0,5");
+	 while ($value = $_SGLOBAL['db']->fetch_array($query)) {
+					//realname_set($value['uid'], $value['username']);
+					$sitefeed_list[] = $value; 		
+	 };
+
+	swritefile($cachefile, serialize($sitefeed_list));
+
+}
+
+function check_feed_cache() {
+		global $_SGLOBAL;
+
+		$cachefile = S_ROOT.'./data/cache_feed.txt';
+		$ftime = filemtime($cachefile);
+		//5 minutes
+		if($_SGLOBAL['timestamp'] - $ftime < 5*60) {
+			return true;
+		}
+		return false;
+}
 ?>
