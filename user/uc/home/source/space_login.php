19ab6deecdcf5cc95cef0014992485c1a507ca30
ramen.sh <ramen.sh@gmail.com>
2010-07-08 13:12:56 +0000

    edit

--- user/uc/home/source/space_login.php
+++ user/uc/home/source/space_login.php
@@ -17,7 +17,7 @@ include_once(S_ROOT.'./source/space_bookmark_show.php');
 
 //今日热荐
 include_once(S_ROOT.'./source/todayhotcollect.php');
-
+include_once(S_ROOT.'./source/sitefeed.php');
 //最后登录名
 $membername = empty($_SCOOKIE['loginuser'])?'':sstripslashes($_SCOOKIE['loginuser']);
 $wheretime = $_SGLOBAL['timestamp']-3600*24*30;
