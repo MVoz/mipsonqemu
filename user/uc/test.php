1f21c10fcacac1ae68adc7b3b64a47644a697735
ramen.sh <ramen.sh@gmail.com>
2010-08-20 09:03:20 +0000

    edit

--- user/uc/test.php
+++ user/uc/test.php
@@ -23,12 +23,17 @@ echo HELLO;
 
 echo addslashes("123'123");
 echo htmlspecialchars("123<123");
+echo '<br>';
+$txt="你好，中国";
 
+echo mb_strlen($txt,"utf-8");
 
+echo mb_substr($txt,0,4,"UTF-8");
 //http://nowdownloadall.com/join.asp?PID=0cdfe76c-1849-49fd-acb3-44e095532314&ts=6/1/2010%2010:28:27%20PM&q=Understanding%20IPv6%20zip&cr=1
 
 //http://quickrpickr.com/ 
 
 //img src="http://lh4.ggpht.com/_QeLyLM_38SU/TAX3i8Jzg9I/AAAAAAAAAGo/r8QD8uaZSZs/s800/1.jpg" />
 //http://hi.baidu.com/viste_happy/blog/item/699a07fc72e84849d7887d2a.html
+//http://www.bram.us/demo/projects/jsprogressbarhandler/
 ?> 
