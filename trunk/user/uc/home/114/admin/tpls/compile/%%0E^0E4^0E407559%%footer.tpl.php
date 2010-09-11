eab96804585d4625d99e4ef80a1b052c337dd583
ramen.sh <ramen.sh@c6258d96-f10f-11de-b2c3-d77de7315633>
2010-09-11 06:53:16 +0000

    edit

---
+++ trunk/user/uc/home/114/admin/tpls/compile/%%0E^0E4^0E407559%%footer.tpl.php
@@ -0,0 +1,9 @@
+<?php /* Smarty version 2.6.25, created on 2010-09-05 22:45:34
+         compiled from footer.tpl */ ?>
+<p style="text-align:center;padding:15px;color:#6B7355;
+font-family:Verdana;
+font-size:12px;">
+Powered by <a href="http://www.ylmf.com" target="_blank"> <b>雨林木风</b></a> &copy; 2009 <a href="http://www.114la.com" target="_blank"> <b>114la.com</b></A><a href="http://www.ylmf.com" target="_blank"></a>
+</p>
+</body>
+</html>
