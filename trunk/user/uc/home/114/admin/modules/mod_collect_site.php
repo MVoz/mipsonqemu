f3198593144400c9d2913611e9491a319d1553fe
ramen.sh@gmail.com <ramen.sh@gmail.com@c6258d96-f10f-11de-b2c3-d77de7315633>
2010-09-13 07:37:41 +0000

    edit

--- trunk/user/uc/home/114/admin/modules/mod_collect_site.php
+++ trunk/user/uc/home/114/admin/modules/mod_collect_site.php
@@ -1,269 +1,396 @@
 <?php
 /**
- * 名站导航切换栏
+ * 网站管理类
  *
- * @since 2009-7-11
  * @copyright http://www.114la.com
+ * @version    $Id: mod_site_manage.php 1541 2009-12-11 07:54:41Z syh $
  */
 !defined('PATH_ADMIN') &&exit('Forbidden');
 class mod_collect_site
 {
-/**
- * 名站导航 列表
- * @return <type>
- */
-	public static function collect_site_list()
+	/**
+	 * 返回搜索结果
+	 *
+	 * @param string $keyword
+	 * @param string $search_type
+	 * @param int $start[optional]
+	 * @param int $num[optional]
+	 * @return array
+	 */
+	public static function search($keyword, $search_type, $start = 0, $num = 0)
 	{
-	//	$timestamp=time();
-	//	$starttime=get_date($timestamp,'Y-m-d');
-	//	$endtime=get_date($timestamp+86400*30,'Y-m-d');
-		$sitedb=array();
-		$query = app_db::query("SELECT * FROM ylmf_link where siteid=0 order by link_dateline DESC  ");
-		$sitedb=array();
-		while ($site = app_db::fetch_one())
+		if ($search_type != 'url' && $search_type != 'name')
 		{
-			$site['dateline']=date( 'Y-m-d',$site['link_dateline']);
-			$site['name']=$site['link_subject'];
-			$site['id']=$site['linkid'];
-			/*
-			if ($site['starttime']==0)
-			{
-				$site['starttime']='-';
-			}else
-			{
-				$site['starttime']=date( 'Y-m-d',$site['starttime']);
-			}
-                        
-			if($site['starttime']==0)
-			{
-				$site['endtime']='-';
-			}elseif ($timestamp>$site['endtime'])
-			{
-				$site['endtime']='<font color=red>'.date('Y-m-d', $site['endtime']).'</font>';
-			}else
-			{
-				$site['endtime']=date( 'Y-m-d',$site['endtime']);
-			}
-			*/
-
-			if(!preg_match("/^https?:\/\/[a-zA-Z0-9~`!@#$%^&*()_+|\\{}[\]:;<?,-.=']+$/", $site['url'])) continue;
-			$sitedb[] = $site;
+			return false;
 		}
-		return $sitedb;
+
+		$condition = " AND a.`{$search_type}` LIKE '%{$keyword}%'";
+		if ($start > -1 && $num > 0)
+		{
+			$condition .= " LIMIT {$start}, {$num}";
+		}
+
+	    $sql = 'SELECT SQL_CALC_FOUND_ROWS a.id, b.classid AS class_id, b.classname AS class_name, a.name, a.url, a.displayorder,
+                       a.gooddisplayorder, a.starttime, a.endtime, a.namecolor, a.adduser, a.good
+                FROM ylmf_site AS a, ylmf_siteclass AS b
+                WHERE a.class = b.classid ' . $condition;
+		$query = app_db::query($sql);
+
+
+		$data = array();
+		while ($rt = app_db::fetch_one($query))
+		{
+		    $data[] = $rt;
+		}
+		if (empty($data))
+		{
+			return false;
+		}
+		/*
+		$data = app_db::select('ylmf_site', 'SQL_CALC_FOUND_ROWS *', $contition);
+		if (empty($data))
+		{
+			return false;
+		}*/
+
+		$output = array();
+		$total = app_db::query('SELECT FOUND_ROWS() AS rows');
+		$total = app_db::fetch_one();
+		$output['total'] = $total['rows'];
+		$output['data'] = $data;
+		return $output;
 	}
 
-	/**
-	 * 添加信息
-	 * @param <array> $data  添加的POST数据
-	 */
-	public static function collect_site_add($data)
-	{
-//		$data=array(
-//			'name'=>'wushc测试',
-//			'namecolor'=>'232323',
-//			'displayorder'=>'232',
-//			'url'=>'http://www.baidu.com',
-//			'starttime'=>'121212121',
-//			'endtime'=>'131313131',
-//			'remark'=>'remak_info___detail',
-//		);
-		if(empty($data['name']))
-		{
-			throw new Exception("请输入站点名称");
-		}
-		$data['name']=Char_cv($data['name']);
-		$data['namecolor']=trim($data['namecolor']);
-		if ($data['namecolor']!='')
-		{
-			if(!eregi("^#?([a-f]|[A-F]|[0-9]){3}(([a-f]|[A-F]|[0-9]){3})?$" ,$data['namecolor']) )
-			{
-				throw new Exception("颜色非法");
-			}
-		}
-		if(!eregi("^http://",$data['url']))
-		{
-			throw new Exception("网站地址不能为空或请以http://开头");
-		}
-		$data['displayorder']=trim($data['displayorder']);
-		!is_numeric($data['displayorder']) && $data['displayorder']=100;
-		$data['starttime']=trim($data['starttime']);
-		$data['endtime']=trim($data['endtime']);
-		$data['starttime']=self::to_timestamp($data['starttime']);
-		$data['endtime']=self::to_timestamp($data['endtime']);
-		if ($data['endtime']<$data['starttime'])
-		{
-			throw new Exception("结束时间不能小于开始时间.");
-		}
-		$data['remark']=char_cv(trim($data['remark']));
-		app_db::query("INSERT INTO ylmf_mingzhan(name,url,namecolor,displayorder,starttime,endtime,remark)VALUES('{$data['name']}','{$data['url']}',
- '{$data['namecolor']}','{$data['displayorder']}','{$data['starttime']}','{$data['endtime']}','{$data['remark']}')");
-		return true;
-	}
 
 	/**
-	 * 删除,接受数组格式
-	 * @param <array> $data
+	 * 获取列表
+	 *
+	 * @param int $class_id
+	 * @param int $isend
+	 * @param int $start[optional]
+	 * @param int $num[optional]
+	 * @return array
 	 */
-	public static function collect_site_delete($delid)
+	public static function get_list($class_id = 0, $isend = false, $start = 0, $num = 20)
 	{
-		if(!is_array($delid))
-		{
-			throw new Exception("请选择删除数据.");
-		}
-		if($delid=checkselid($delid))
-		{
-			app_db::query("DELETE FROM ylmf_link WHERE id IN ($delid) ");
-			self::updatecache_classnum();//更新缓存
-			return true;
-		}else
-		{
-			throw new Exception("参数非法.");
-		}
-	}
+		$condition = '';
 
-	/**
-	 * 保存数据
-	 * @param <array> $data
-	 */
-	public static function collect_site_save($data,$type='select')
-	{
-//		$data=array(
-//			'step'=>'2',
-//			'id'=>'2',
-//			'name'=>'腾讯QQQ',
-//			'namecolor'=>'232323',
-//			'displayorder'=>'2',
-//			'url'=>'http://www.baidu.com',
-//			'starttime'=>'121212121',
-//			'endtime'=>'131313131',
-//			'remark'=>'remak_info___detail',
-//		);
-		if(!is_numeric($data['id']))
+		$cache_main_class = mod_class::get_class_list();
+		/*
+		// 分类
+		if ($class_id > 0)
 		{
-			throw new Exception("非法参数");
-		}
-		$data['id']=intval($data['id']);
-		$info=app_db::query("SELECT * FROM ylmf_link WHERE linkid='{$data['id']}' ");
-                $info=app_db::fetch_one();
-		if(!$info)
-		{
-			throw new Exception("没有这个站点");
-		}
-		if ($type=='save')//保存信息
-
-		{
-			$data['name']=trim($data['name']);
-			if($data['name']=='')
+			if ($cache_main_class[$class_id]['parentid'] == 0)
 			{
-				throw new Exception("请输入站点名称");
-			}
-			$data['name']=Char_cv($data['name']);
-			$data['namecolor']=trim($data['namecolor']);
-			if ($data['namecolor']!='')
-			{
-				if(!eregi("^#?([a-f]|[A-F]|[0-9]){3}(([a-f]|[A-F]|[0-9]){3})?$" ,$data['namecolor']) )
+				$tmp = '';
+				foreach ($cache_main_class as $key => $val)
 				{
-					throw new Exception("颜色非法");
+					if (!empty($cache_main_class[$val['parentid']]) && $cache_main_class[$val['parentid']]['parentid'] == $class_id)
+					{
+						$tmp .= $key . ',';
+					}
+				}
+				if ($tmp != '')
+				{
+					$tmp = substr($tmp, 0, -1);
+					$condition .= (!empty($condition)) ? " AND `class` IN({$tmp})" :
+														" AND `class` IN({$tmp})";
 				}
 			}
-			if(!eregi("^http://",$data['url']))
+			//2级分类
+			elseif ($cache_main_class[$cache_main_class[$class_id]['parentid']]['parentid'] == 0)
 			{
-				throw new Exception("网站地址不能为空或请以http://开头");
+				$tmp = '';
+				foreach ($cache_main_class as $key => $val)
+				{
+					if ($val['parentid'] == $class_id)
+					{
+						$tmp .= $key . ',';
+					}
+				}
+				if ($tmp != '')
+				{
+					$tmp = substr($tmp, 0, -1);
+					$condition .= (!empty($condition)) ? " AND `class` IN({$tmp})" :
+														" AND `class` IN({$tmp})";
+				}
 			}
-			$data['displayorder']=trim($data['displayorder']);
-			!is_numeric($data['displayorder']) && $data['displayorder']=100;
-			$data['starttime']=trim($data['starttime']);
-			$data['endtime']=trim($data['endtime']);
-			$data['starttime']=self::to_timestamp(($data['starttime']));
-			$data['endtime']=self::to_timestamp(($data['endtime']));
-			if ($data['endtime']<$data['starttime'])
+			//3级分类
+			else
 			{
-				throw new Exception("结束时间不能小于开始时间.");
+				$condition .= (!empty($condition)) ? " AND `class` = {$class_id}" :
+													" AND `class` = {$class_id}";
 			}
-			$data['remark']=char_cv(trim($data['remark']));
-			app_db::query("UPDATE ylmf_mingzhan SET name='{$data['name']}',url='{$data['url']}' ,namecolor='{$data['namecolor']}',starttime='{$data['starttime']}',
-				displayorder='{$data['displayorder']}',endtime='{$data['endtime']}',remark='{$data['remark']}' WHERE id='{$data['id']}' ");
-			return true;
 		}
-		elseif($type=='select')
+
+		// 过期
+		if ($isend)
 		{
-			$info['name']=trim($info['link_subject']);
-			$info['remark']=trim($info['link_description']);
-			/*
-			if ($info['starttime']==0)
-			{
-				$info['starttime']='';
-			}else
-			{
-				$info['starttime']=get_date( $info['starttime'],'Y-m-d');
-			}
-			if ($info['endtime']==0)
-			{
-				$info['endtime']='';
-			}else
-			{
-				$info['endtime']=get_date( $info['endtime'],'Y-m-d');
-			}
-			*/
-			return $info;
+			$condition .= (!empty($condition)) ? ' AND `endtime` > 0 AND `endtime` < ' . time() :
+												 ' AND `endtime` > 0 AND `endtime` < ' . time();
 		}
+		*/
+		$condition .= ' ORDER BY link_dateline DESC';
+		if ($start > -1 && $num > 0)
+		{
+			$condition .= " LIMIT {$start}, {$num}";
+		}
+
+	    $sql = 'SELECT SQL_CALC_FOUND_ROWS a.linkid AS id,  a.link_subject as name, a.url, 
+                       a.link_dateline as time, a.username as adduser
+                FROM ylmf_link AS a WHERE a.siteid=0 ' . $condition;
+		$query = app_db::query($sql);
+
+		$data = array();
+		while ($rt = app_db::fetch_one())
+		{
+			if(!preg_match("/^https?:\/\/[a-zA-Z0-9~`!@#$%^&*()_+|\\{}[\]:;<?,-.=']+$/", $rt['url'])) continue;
+		    $data[] = $rt;
+		}
+		if (empty($data))
+		{
+			return false;
+		}
+
+		$output = array();
+		$total = app_db::query('SELECT FOUND_ROWS() AS rows');
+		$total = app_db::fetch_one();
+		$output['total'] = $total['rows'];
+		$output['data'] = $data;
+		return $output;
 	}
 
+
 	/**
-	 * 排序保存
-	 * @param <type> $orderby
+	 * 获取一个站点的信息
 	 */
-	public static function collect_site_order($orderby)
+	public static function get_one($id)
+	{
+		if ($id < 1)
+		{
+			return false;
+		}
+		$id = (int)$id;
+
+		$data = app_db::select('ylmf_link', '*', "linkid = {$id}");
+
+		return (empty($data)) ? false : $data[0];
+	}
+
+
+	/**
+	 * 检查网站是否已存在
+	 *
+	 * @param int $class_id
+	 * @param int $site 网站名或 URL
+	 */
+	public static function check_exists($class_id, $site)
+	{
+		$class_id = (int)$class_id;
+		$data = app_db::select('ylmf_site', 'id', "`class` = {$class_id} AND (`name` = '{$site}' OR url = '{$site}')");
+		return (empty($data)) ? false : true;
+	}
+
+
+	/**
+	 * 删除分类的所有网站
+	 *
+	 * @param int $class_id 分类ID
+	 */
+	public static function delete_by_class($class_id)
+	{
+		$class_id = (int)$class_id;
+		if ($class_id < 1)
+		{
+			return false;
+		}
+
+		return app_db::delete('ylmf_site', "class = {$class_id}");
+	}
+
+
+	/**
+	 * 批量添加网站
+	 *
+	 * @param array $sites 网址数组
+	 */
+	public static function multi_add($sites)
+	{
+        if(is_array($sites) && !empty($sites))
         {
-            if (is_array($orderby))
+            $class = intval($sites['classid']);
+            if(!empty($sites['sites']))
             {
-                foreach($orderby as $key =>$val)
+                foreach($sites['sites'] as $site)
                 {
-                    $order=(int)$val;
-                    $id=intval($key);
-                    app_db::query("UPDATE ylmf_mingzhan SET displayorder='$order' WHERE id=$key");
+                    $name = htmlspecialchars(trim($site['name']));
+                    $url = htmlspecialchars(trim($site['url']));
+                    if(empty($name) || empty($url))
+                    {
+                        continue;
+                    }
+                    $remark = htmlspecialchars(trim($site['remark']));
+                    app_db::query("INSERT INTO `ylmf_site` ( `name` , `url` , `class` , `remark` ) VALUES ( '$name', '$url', '$class', '$remark')");
                 }
             }
-            else
-            {
-                throw new Exception("提交排序参数错误,请重试.");
-            }
         }
-
-
-
-	/**
-	 * 更新站点分类
-	 * @global <type> $db
-	 */
-	public static function   updatecache_classnum()
-	{
-		global  $db;
-		$data=app_db::select("ylmf_siteclass" , "*","1");
-		foreach ($data as $class)
-		{
-			app_db::query("SELECT count(*) as sum FROM ylmf_site WHERE class='$class[classid]'");
-			$rt=app_db::fetch_one();
-			app_db::query("UPDATE ylmf_siteclass SET sitenum='$rt[sum]'  WHERE classid='$class[classid]'");
-		}
 	}
 
-        public static function to_timestamp($Time,$Which="-",$Long=6)
+    /**
+	 * 批量导入网站
+	 *
+	 * @param array $data 网址数组
+	 */
+	public static function import($data)
+	{
+        $sites['classid'] = $data['classid'];
+        //解析字符串
+        preg_match_all("/<a.*?href=.?[\'\"](.*?).?[\'\"].*?>(.*?)<\/a>/is", $data['sites'], $result);
+        foreach($result[2] as $i => $name)
         {
-            if(strlen($Time)>$Long)
-            {	$timeexplode=explode($Which,$Time);
-                return mktime(0, 0, 0, $timeexplode[1], $timeexplode[2], $timeexplode[0]);
+            $name = trim(strip_tags($name));
+            $url = $result[1][$i];
+            if(empty($name) || empty($url))
+            {
+                unset($result[1][$i]);
+                unset($result[2][$i]);
+                continue;
             }
-            else
-            {	return false;
-            }
+            $sites['sites'][] = array('name' => $name, 'url' => $url, 'remark' => '');
         }
-        public static function from_timestamp($Time,$How=10000000)
-        {
-            if($Time>$How)
-                return date("Y-m-d",$Time);
-            else
-                return null;
-        }
+        self::multi_add($sites);
+	}
+	public static function simplode($ids) {
+		return "'".implode("','", $ids)."'";
+	}
+	public static function getUnicodeFromOneUTF8($word) {   
+	  if (is_array( $word))   
+		$arr = $word;   
+	  else     
+		$arr = str_split($word);    
+	  $bin_str = '';   
+	  foreach ($arr as $value)   
+		$bin_str .= decbin(ord($value));   
+	  $bin_str = preg_replace('/^.{4}(.{4}).{2}(.{6}).{2}(.{6})$/','$1$2$3', $bin_str);   
+	  return bindec($bin_str); 
+	}
+	public static function mbStringToArray ($string) {
+		$strlen = mb_strlen($string);
+		while ($strlen) {
+			$array[] = mb_substr($string,0,1,"UTF-8");
+			$string = mb_substr($string,1,$strlen,"UTF-8");
+			$strlen = mb_strlen($string);
+		}
+		return $array;
+	}
+	public static function _qhash($p, $n)
+	{
+		$h = 0;
+		$g;
+		  $i=0;
+		while ($n--) {
+			$h = (($h) << 4) + self::getUnicodeFromOneUTF8($p[$i]);
+			if (($g = ($h & 0xf0000000)) != 0)
+				$h ^= $g >> 23;
+			$h &= ~$g;
+					$i++;
+		}
+		return $h;
+	}
+
+	public static function qhash($str){
+	  $t=self::mbStringToArray($str);
+	  return self::_qhash($t,count($t));
+	}
+	/**
+		process tag
+	**/
+	public static function batch_tag($id,$tags)
+	{
+		$mtime = explode(' ', microtime());
+		$timesec = $mtime[1];
+		$tagarr = array();
+		$now_tag = empty($tags)?array():array_unique(explode(' ', $tags));
+		
+		//if(empty($now_tag)) return $tagarr;
+		//获取原来的tags
+		 $result = self::get_one($id);
+	
+         if (empty($result))
+         {
+              throw new Exception('没有找到数据', 10);
+         }
+		
+		 //修正tag显示
+		 $old_tags = empty($result['tag'])?array():unserialize($result['tag']);
+		
+		 $need_delete_tags=array();
+		 $need_add_tags=array();
+		 $tagarr = $intersect_tag = array_intersect($old_tags,$now_tag);
+		//获取old_tag有而现在没有的			
+		 $need_delete_tags = array_diff($old_tags,$intersect_tag);
+		//清除tag
+		if(!empty($need_delete_tags)) {
+			  foreach($need_delete_tags as $k=>$v){
+				 app_db::query("DELETE  from ylmf_sitetagsite WHERE siteid=".$id.' AND tagid='.$k);			
+			  }	  
+			 app_db::query("UPDATE ylmf_sitetag SET totalnum=totalnum-1 WHERE tagid IN (".self::simplode(array_keys($need_delete_tags)).")");
+		}
+		//获取现在有二old_tag没有的
+		 $need_add_tags =   array_diff($now_tag,$intersect_tag);
+		 
+		 if(empty($need_add_tags))
+			 return  $tagarr;
+		//记录已存在的tag
+		$vtags = array();
+		
+		$sql = "SELECT tagid, tagname, close FROM ylmf_sitetag WHERE tagname IN (".self::simplode($need_add_tags).")";
+		$query = app_db::query($sql);
+	
+		while ($rt = app_db::fetch_one())
+		{
+			$rt['tagname'] = addslashes($rt['tagname']);
+		    $vkey = md5($rt['tagname']);
+			$vtags[$vkey] = $rt;
+		}
+
+		
+		$updatetagids = array();
+		foreach ($need_add_tags as $tagname) {
+			if(!preg_match('/^([\x7f-\xff_-]|\w){3,20}$/', $tagname)) continue;
+			
+			$vkey = md5($tagname);
+			if(empty($vtags[$vkey])) {
+				$setarr = array(
+					'tagname' => $tagname,
+					'taghash' => self::qhash($tagname),
+					'dateline' => $timesec,
+					'totalnum' => 1
+				);
+				if (app_db::insert('ylmf_sitetag', array_keys($setarr), array_values($setarr)))
+				{
+					
+					$tagid = @mysql_result(app_db::query("SELECT last_insert_id()"), 0);
+					$tagarr[$tagid] = $tagname;
+				}
+			} else {
+				if(empty($vtags[$vkey]['close'])) {
+					$tagid = $vtags[$vkey]['tagid'];
+					$updatetagids[] = $tagid;
+					$tagarr[$tagid] = $tagname;
+				}
+			}
+		}
+		if($updatetagids) app_db::query("UPDATE ylmf_sitetag SET totalnum=totalnum+1 WHERE tagid IN (".self::simplode($updatetagids).")");
+		$tagids = array_keys($tagarr);
+		$inserts = array();
+		foreach ($tagids as $tagid) {
+			$inserts[] = "('$tagid','$id')";
+		}
+		if($inserts) app_db::query("REPLACE INTO ylmf_sitetagsite (tagid,siteid) VALUES ".implode(',', $inserts));
+
+		return $tagarr;
+		
+	}
 }
 ?>
