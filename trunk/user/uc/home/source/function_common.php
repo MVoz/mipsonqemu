3aaf5da01eb66694bd6243d98e6c789e50d2ab01
ramen.sh@gmail.com <ramen.sh@gmail.com@c6258d96-f10f-11de-b2c3-d77de7315633>
2010-09-29 03:54:45 +0000

    edit

--- trunk/user/uc/home/source/function_common.php
+++ trunk/user/uc/home/source/function_common.php
@@ -3148,4 +3148,17 @@ function getbookmarkgroupname($browserid,$groupid){
 	}
 	return $groupname;
 }
+function deldir($dir){
+	 $dh=opendir($dir);
+	 while ($file=readdir($dh)){
+		  if($file!="."&&$file!=".."){
+			   $fullpath=$dir."/".$file;
+				if(!is_dir($fullpath))
+					unlink($fullpath);
+				else
+					deldir($fullpath);   
+			  }
+		 }
+	 closedir($dh);
+}
 ?>
