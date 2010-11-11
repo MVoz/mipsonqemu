e1240b29b749a1a5f02f61672c1b4caf26eee88e
ramen.sh@gmail.com <ramen.sh@gmail.com@c6258d96-f10f-11de-b2c3-d77de7315633>
2010-11-11 10:41:22 +0000

    edit

--- trunk/user/uc/home/source/space_login.php
+++ trunk/user/uc/home/source/space_login.php
@@ -20,7 +20,7 @@ include_once(S_ROOT.'./source/todayhotcollect.php');
 include_once(S_ROOT.'./source/sitefeed.php');
 //最后登录名
 $membername = empty($_SCOOKIE['loginuser'])?'':sstripslashes($_SCOOKIE['loginuser']);
-$wheretime = $_SGLOBAL['timestamp']-3600*24*30;
+//$wheretime = $_SGLOBAL['timestamp']-3600*24*30;
 if(!empty($_SCOOKIE['cookiecheck']))
 	$cookiecheck = ' checked';
 $_TPL['css'] = 'network';
