6e1a99ed64f4ac9ece239ddf1215a2fc912809b1
ramen.sh <ramen.sh@gmail.com>
2010-12-20 08:24:29 +0000

    edit

--- user/uc/home/source/space_bookmark.php
+++ user/uc/home/source/space_bookmark.php
@@ -15,13 +15,5 @@ if(empty($_SGLOBAL['supe_uid'])) {
 include_once(S_ROOT.'./source/every_highlight.php');
 include_once(S_ROOT.'./source/space_bookmark_show.php');
 
-//今日热荐
-include_once(S_ROOT.'./source/every_todayhot.php');
-
-//最后登录名
-//$membername = empty($_SCOOKIE['loginuser'])?'':sstripslashes($_SCOOKIE['loginuser']);
-//$wheretime = $_SGLOBAL['timestamp']-3600*24*30;
-
-$_TPL['css'] = 'network';
 include_once template("space_bookmark");
 ?>
--- user/uc/home/source/space_digg.php
+++ user/uc/home/source/space_digg.php
@@ -14,12 +14,7 @@ if(empty($_SCONFIG['networkpublic'])) {
 }
 
 include_once(S_ROOT.'./source/every_highlight.php');
-include_once(S_ROOT.'./data/data_network.php');
 
-$type = empty($_GET['type'])?0:intval($_GET['type']);
-//检查type合法性
-if(!isbetween($type,0,count($_SGLOBAL['diggcategory'])))
-	  $type=0;
 //根据postuser来查看
 $uid = empty($_GET['uid'])?0:intval($_GET['uid']);
 //显示数量
@@ -31,79 +26,7 @@ else
 //获取总条数
 $page=empty($_GET['page'])?0:intval($_GET['page']);
 $perpage=$shownum;
-$start=$page?(($page-1)*$perpage):0;
-//$theurl="space.php?do=$do&show=".$shownum;
-$theurl="diggpage";
-//digg
-$digglist = array();
-
-//修正一下page ，0不变，其它情况减1
-$nowpage=($page)?($page-1):0;
-
-if($type)
-	{
-		$cachefile = S_ROOT.'./data/diggcache/data_diggcache_'.$type.'_'.$nowpage.'.php';
-		$count=sreadfile(S_ROOT.'./data/diggcache/digg_'.$type.'_count.txt');
-		$theurl = $theurl.'&type='.$type;
-	}
-else if($uid){
-		$cachefile = S_ROOT.'./data/diggcache/data_diggcache_user_'.$uid.'_'.$nowpage.'.php';
-		$count=sreadfile(S_ROOT.'./data/diggcache/digg_user_'.$uid.'_count.txt');
-		$theurl = $theurl.'&uid='.$uid;
-}
-else {
-	$cachefile = S_ROOT.'./data/diggcache/data_diggcache'.$nowpage.'.php';
-	$count=sreadfile(S_ROOT.'./data/diggcache/digg_count.txt');
-}
-if(!check_cachelock('digg')&&file_exists($cachefile)) {
-	//没有lock,则可以读取
-	include_once($cachefile);		
-	if($shownum!=$_SC['digg_show_maxnum'])
-	{
-		$realpage = floor((($nowpage+1)*$shownum)/$_SC['digg_show_maxnum']);
-		$digglist = $_SGLOBAL['diggcache'][$realpage];
-		//偶数去前8个，奇数取后8个
-		if($page==0)
-			$page = 1;
-		$tmpdigglist = array_chunk($digglist, $shownum);
-		if($page%2)//奇数
-		{
-			//取前8个
-			$digglist = $tmpdigglist[0];
-		}else{
-			//移除前8个
-			$digglist = $tmpdigglist[1];
-		}
-	}else{
-		$digglist = $_SGLOBAL['diggcache'][$nowpage];
-	}
-} else {
-   
-	$wherearr='';
-	if(!empty($type))
-		 $wherearr=' where main.categoryid='.$type;
-	if(!empty($uid))
-		 $wherearr=' where main.postuid='.$uid;
-
-    $count = $_SGLOBAL['db']->result($_SGLOBAL['db']->query("SELECT COUNT(*) FROM ".tname('digg')),0);
-	$query = $_SGLOBAL['db']->query("SELECT main.*	FROM ".tname('digg')." main ".$wherearr." ORDER BY main.dateline LIMIT $start,$shownum");
-
-	while ($value = $_SGLOBAL['db']->fetch_array($query)) {
-		$value['subject'] = getstr($value['subject'], 50);
-		$value['cutsubject'] = getstr(trim($value['subject']), 28);
-		$digglist[] = $value;
-	}
-	if($_SGLOBAL['network']['digg']['cache']) {
-		swritefile($cachefile, serialize($digglist));
-	}
-}
-foreach($digglist as $key => $value) {
-	//realname_set($value['uid'], $value['username']);
-	$value['tag'] = empty($value['tag'])?array():unserialize($value['tag']);
-	$digglist[$key] = $value;
-}
-//分页
-$diggmulti = multi($count, $perpage, $page, $theurl,'diggcontent','diggcontent',1);
+include_once(S_ROOT.'./source/space_digg_show.php');
 
 include_once template("space_digg");
 
