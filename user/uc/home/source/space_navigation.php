197a45c03fd16dacb82380faac24c38ae63cdd06
ramen.sh <ramen.sh@gmail.com>
2010-12-21 06:34:45 +0000

    edit

--- user/uc/home/source/space_navigation.php
+++ user/uc/home/source/space_navigation.php
@@ -82,38 +82,11 @@ if($classid)
 			$bookmarklist = unserialize(sreadfile(S_ROOT.'./data/sitecache/'.$browserclassid.'/site_cache_'.$browserclassid.'_page'.$page.'.txt'));
 			$count=sreadfile(S_ROOT.'./data/sitecache/'.$browserclassid.'/site_cache_'.$browserclassid.'_count.txt');
 		}
-
-/*	
-		if(($classitem['groupid']<3000)&&($classitem['groupid']>=2000))	//判断是否为第二层
-		  		$isSecClass=1;
-		//获取子分类
-		$query=$_SGLOBAL['db']->query("SELECT main.* FROM ".tname('linkclass')." main where main.parentid=".$classitem['groupid']);
-		while($value =$_SGLOBAL['db']->fetch_array($query))
-		{
-			$child_class[]=$value;
-		}
-		//获取下面的link信息
-		if($childid)
-		{
-		}else{
-			//修正page
-			//$page=$page?($page-1):$page;
-			//先检查cache
-			if(!file_exists( S_ROOT.'./data/linkcache/'.$classid.'/link_cache_'.$classid.'_page'.$page.'.txt'))
-			{
-				include_once(S_ROOT.'./source/function_cache.php');
-				link_cache_classid($classid);				
-			}
-			$bookmarklist = unserialize(sreadfile(S_ROOT.'./data/linkcache/'.$classid.'/link_cache_'.$classid.'_page'.$page.'.txt'));
-			$count=sreadfile(S_ROOT.'./data/linkcache/'.$classid.'/link_cache_'.$classid.'_count.txt');
-		}
-*/
 	}
 }else{
 	//显示导航
 	//include_once(S_ROOT.'./source/space_bookmark_show.php');
-	//获取显示的nav分类
-	
+	//获取显示的nav分类	
 	if(!file_exists($S_ROOT.'./data/navigation_cache.txt'))
 	{
 		include_once($S_ROOT.'./source/function_cache.php');
@@ -127,46 +100,10 @@ if(!file_exists($S_ROOT.'./data/navigation_siteclass.txt'))
 	include_once($S_ROOT.'./source/function_cache.php');
 	siteclass_cache();			
 }
-$siteclass = unserialize(sreadfile($S_ROOT.'./data/navigation_siteclass.txt'));
-
-
-//获取书签分类列表
 //获取class分类
-/*
-$class_query  = $_SGLOBAL['db']->query("SELECT main.* FROM ".tname('linkclass')." main WHERE main.parentid=0");
-while($value =$_SGLOBAL['db']->fetch_array($class_query))
-{
-		//获取二级目录
-		$classnd_query  = $_SGLOBAL['db']->query("SELECT main.* FROM ".tname('linkclass')." main WHERE main.parentid=".$value['groupid']);
-		while($classnd_value =$_SGLOBAL['db']->fetch_array($classnd_query))
-		{
-			$value['son'][]=$classnd_value;
-		}
-	$linkclasslist[]=$value;	
-}
-*/
-//realname_get();
-
-//修正tag以便显示
-/*
-foreach($bookmarklist as $key => $value) {
-	//realname_set($value['uid'], $value['username']);
-	//include_once(S_ROOT.'./source/function_link.php');
-	//$value['link_tag']=convertlinktag($value['linkid'],$value['link_tag']);
-	//if($value[picflag]&&empty($value['tag']))
-	//		$value['tag']= $value['link_tag'];
-	$value['tag'] = empty($value['tag'])?array():unserialize($value['tag']);
-	$bookmarklist[$key] = $value;	
-}
-*/
-//$theurl="space.php?do=$do&classid=$classid&childid=$childid";
+$siteclass = unserialize(sreadfile($S_ROOT.'./data/navigation_siteclass.txt'));
 $theurl="sitepage";
 //分页
 $bookmark_multi = multi($count, $perpage, $page, $theurl,'bmcontent','bmcontent',1,$classid.'|'.$childid.'|');
-//$_TPL['css'] = 'network';
 include_once template("space_navigation");
-/*
-}
-*/
-
 ?>
