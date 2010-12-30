e0dff53654f4a2b0b28ec91a3e4685d90637611a
ramen.sh <ramen.sh@gmail.com>
2010-12-30 06:08:00 +0000

    edit

--- user/uc/home/tool/timepost.c
+++ user/uc/home/tool/timepost.c
@@ -11,6 +11,7 @@
 
 CURL *easy_handle=NULL;
 FILE *fp = NULL;
+unsigned int maxinterval=100;
 #define MAX_TIME_INTERVAL 100 
 char *httpserver = "http://192.168.115.2/";
 char *robotid = "f69c1725e2a57ec91d1c89d452396c1f";
@@ -27,13 +28,82 @@ struct timer_action
 	char **ac;
 	int time[2];
 };
-struct timer_action a[]={
+struct timer_action timer_actions[]={
 	{&action[0],{8,24}},
-	{&action[1],{8,24}},
+	{&action[3],{0,24}},
+	{&action[4],{6,23}},
 	{&action[2],{0,24}},
+	{&action[1],{8,24}},
+	{&action[3],{0,24}},
+	{&action[4],{6,23}},
+	{&action[2],{0,24}},
+	{&action[3],{0,24}},
+	{&action[2],{0,24}},
+	{&action[4],{6,23}},
 	{&action[3],{0,24}},
 	{&action[4],{6,23}}
 };
+/**
+ *	@brief libcurl接收到数据时的回调函数
+ *
+ *	将接收到的数据保存到本地文件中，同时显示在控制台上。
+ *
+ *	@param [in] buffer 接收到的数据所在缓冲区
+ *	@param [in] size 数据长度
+ *	@param [in] nmemb 数据片数量
+ *	@param [in/out] 用户自定义指针
+ *	@return 获取的数据长度
+ */
+
+size_t process_data(void *buffer, size_t size, size_t nmemb, void *user_p)
+{
+//	FILE *fp = (FILE *)user_p;
+//	size_t return_size = fwrite(buffer, size, nmemb, fp);
+	printf("buffer=%s\n\n",buffer);
+	return 0;
+}
+int sighandler_timer(int signo)
+{
+	// 设置easy handle属性
+	time_t now;
+	struct tm  *nowtime;
+	now = time(NULL);
+	nowtime = localtime(&now);
+//	printf("Local time is: %s", asctime(nowtime));
+	
+	int action_r = sizeof(timer_actions)/sizeof(struct timer_action);
+	
+//	printf("action_r =%d\n",action_r);
+	srand((int)time(0));
+	int t=(int)(((float)(action_r))*rand()/(RAND_MAX+1.0));
+
+	if((nowtime->tm_hour>=timer_actions[t].time[0])&&(nowtime->tm_hour<=timer_actions[t].time[1]))
+	{
+		char url[1024]={0};
+		strcpy(url,httpserver);
+		strcat(url,*(timer_actions[t].ac));
+		
+		char *p = strchr(url,'#');
+		if(p){
+			sprintf(p,"%d",1);
+		}
+		sprintf(url,"%s&robot=%s",url,robotid);
+		printf("url=%s\n",url);
+		curl_easy_setopt(easy_handle, CURLOPT_URL,url);
+		curl_easy_setopt(easy_handle, CURLOPT_WRITEFUNCTION, &process_data);
+		curl_easy_setopt(easy_handle, CURLOPT_WRITEDATA, fp);
+
+		// 执行数据请求
+		curl_easy_perform(easy_handle);	
+	}
+	// 释放资源
+	//随机定时
+	printf("maxinterval=%u\n",maxinterval);
+	srand((int)time(0));
+	int n = MAX_TIME_INTERVAL;
+	t=5+(int)(((float)(n))*rand()/(RAND_MAX+1.0));
+	alarm(t);
+}
 pid_t pid, sid;
 
 void init_daemon(void)
@@ -72,78 +142,22 @@ void init_daemon(void)
 }
 
 
-/**
- *	@brief libcurl接收到数据时的回调函数
- *
- *	将接收到的数据保存到本地文件中，同时显示在控制台上。
- *
- *	@param [in] buffer 接收到的数据所在缓冲区
- *	@param [in] size 数据长度
- *	@param [in] nmemb 数据片数量
- *	@param [in/out] 用户自定义指针
- *	@return 获取的数据长度
- */
 
-size_t process_data(void *buffer, size_t size, size_t nmemb, void *user_p)
-{
-//	FILE *fp = (FILE *)user_p;
-//	size_t return_size = fwrite(buffer, size, nmemb, fp);
-	printf("buffer=%s\n\n",buffer);
-	return 0;
-}
-int sighandler_timer(int signo)
-{
-	// 设置easy handle属性
-	time_t now;
-	struct tm  *nowtime;
-	now = time(NULL);
-	nowtime = localtime(&now);
-	printf("Local time is: %s", asctime(nowtime));
-	
-	int action_r = sizeof(action)/sizeof(char*);
-
-	srand((int)time(0));
-	int t=(int)(((float)(action_r))*rand()/(RAND_MAX+1.0));
-	
-	char url[1024]={0};
-	strcpy(url,httpserver);
-	strcat(url,action[t]);
-	
-	char *p = strchr(url,'#');
-	if(p){
-		sprintf(p,"%d",1);
-	}
-	sprintf(url,"%s&robot=%s",url,robotid);
-	printf("url=%s\n",url);
-	curl_easy_setopt(easy_handle, CURLOPT_URL,url);
-	curl_easy_setopt(easy_handle, CURLOPT_WRITEFUNCTION, &process_data);
-	curl_easy_setopt(easy_handle, CURLOPT_WRITEDATA, fp);
-
-	// 执行数据请求
-	curl_easy_perform(easy_handle);	
-
-	// 释放资源
-	//随机定时
-	srand((int)time(0));
-	int n = MAX_TIME_INTERVAL;
-	t=5+(int)(((float)(n))*rand()/(RAND_MAX+1.0));
-	alarm(t);
-}
 void PrintUsage(int argc, char *argv[]) {
     if (argc >=1) {
-        printf("Usage: %s -h -nn", argv[0]);
+        printf("Usage: %s -h -n\n", argv[0]);
         printf("  Options:n");
-        printf("      -ntDon't fork off as a daemon.n");
-        printf("      -htShow this help screen.n");
+        printf("      -d\tAs a daemon\n");
+		printf("      -m\tSet maxinum interval\n");
+        printf("      -htShow this help screen\n");
         printf("n");
     }
 }
-
 int main(int argc, char **argv)
 {
 	int c;
 	int daemonize = 0;
-    while( (c = getopt(argc, argv, "dh|help")) != -1) {
+    while( (c = getopt(argc, argv, "dhm:|help")) != -1) {
         switch(c){
             case 'h':
                 PrintUsage(argc, argv);
@@ -151,6 +165,9 @@ int main(int argc, char **argv)
                 break;
             case 'd':
                 daemonize = 1;
+                break;
+			case 'm':
+                maxinterval = (unsigned int)strtol(optarg, NULL, 10);
                 break;
             default:
                 PrintUsage(argc, argv);
