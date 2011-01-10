e4ec4e8cee271f5ce8cf798db85ea11c80b47b4a
ramen.sh <ramen.sh@c6258d96-f10f-11de-b2c3-d77de7315633>
2011-01-10 15:49:37 +0000

    edit

---
+++ trunk/user/uc/home/source/space_soft.php
@@ -0,0 +1,14 @@
+<?php
+/*
+	[UCenter Home] (C) 2007-2008 Comsenz Inc.
+	$Id: network_album.php 12078 2009-05-04 08:28:37Z zhengqingpeng $
+*/
+
+if(!defined('IN_UCHOME')) {
+	exit('Access Denied');
+}
+
+include_once(S_ROOT.'./source/every_highlight.php');
+
+include_once template("space_soft");
+?>
