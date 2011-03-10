ee7840b04d61cb1fa32d6a927f4e8a8f5e599320
ramen.sh <ramen.sh@gmail.com>
2011-03-10 06:06:04 +0000

    $logdate

--- user/uc/home/data/data_browser.php
+++ user/uc/home/data/data_browser.php
@@ -2,6 +2,19 @@
 if(!defined('IN_UCHOME')) exit('Access Denied');
 $_SGLOBAL['browser']=Array
 	(
+	0 => Array
+		(
+		'browserid' => '0',
+		'browsername' => 'network',
+		'maxlev' => 5,
+		'maxchild' => 128,
+		'titlelen' => 512,
+		'dirlen' => 256,
+		'urllen' => 512,
+		'taglen' => 128,
+		'deslen' => 512,
+		'speicalchar' => 1
+		),
 	1 => Array
 		(
 		'browserid' => 1,
--- user/uc/home/data/data_browsertype.php
+++ user/uc/home/data/data_browsertype.php
@@ -2,6 +2,7 @@
 if(!defined('IN_UCHOME')) exit('Access Denied');
 $_SGLOBAL['browsertype']=Array
 	(
+	'network' => '0',
 	'ie' => 1,
 	'firefox' => 2,
 	'opera' => 3
