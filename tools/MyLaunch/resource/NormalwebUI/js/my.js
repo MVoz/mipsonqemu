4cfe55e858a317007eb42259f666b060a032422b
ramen.sh <ramen.sh@gmail.com>
2012-08-23 14:23:35 +0000

    new UI

--- tools/MyLaunch/resource/NormalwebUI/js/my.js
+++ tools/MyLaunch/resource/NormalwebUI/js/my.js
@@ -126,9 +126,19 @@ function tb_detectMacXFF() {
 		$("#nav").append('<li><a class="howto" href="net.html">网&nbsp;&nbsp;&nbsp;络</a></li>');
         $("#nav").append('<li><a class="opts" href="custom.html">目录&amp;命令</a></li>');
         $("#nav").append('<li class="fl_r"><a class="buy">&nbsp;</a></li>');
+
+		$('#nav  a').click(function(){
+			OptionsDlg.getHtml(":"+$(this).attr('href'));
+		});
 		//apply
-		$("#apply > ul").append('<li><a href="#">取消</a></li>');
-		$("#apply > ul").append('<li><a href="#">确定</a></li>');		
+		$("#apply > ul").append('<li id="cancel"><a href="#">取消</a></li>');
+		$("#apply > ul").append('<li id="apply"><a href="#">确定</a></li>');	
+		$('#apply #apply a').click(function(){
+			//OptionsDlg.reject();
+		});
+		$('#apply #cancel a').click(function(){
+			OptionsDlg.reject();
+		});
 		//footer
 		$("#footer ul").append('<li>&copy; 2012 - <a href="http://codecanyon.net/user/360north">360north</a> &nbsp;/&nbsp; <a href="http://codecanyon.net/user/360north/follow">Follow on Envato</a></li>');
 
